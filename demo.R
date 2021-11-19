ce6d77953fe7e1cadbd468fc889a8d382404d6b1
rongli6 <lirnishui@gmail.com>
2021-11-19 01:56:29 -0800

    add main

--- demo.R
+++ demo.R
@@ -12,6 +12,11 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+
+main <- function(){
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg4)
+}
+
+main()
